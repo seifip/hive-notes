90bd5346b697eb736a287c94d5734846eb22ff5d
Philip Seifi <seifip@gmail.com>
2020-04-12 15:33:53 +0300

    purgecss fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx', './src/tailwindTheme.js'],
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    './src/tailwindTheme.js'
+  ],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
--- src/tailwindTheme.js
+++ src/tailwindTheme.js
@@ -5,7 +5,9 @@ const TButton = {
 
 const TCard = {
   baseClass: 'bg-primary rounded shadow-md',
-  bodyClass: 'p-5'
+  headerClass: 'px-6 py-3 text-xl border-b',
+  bodyClass: 'p-5',
+  footerClass: 'px-6 py-3 border-t'
 }
 
 const TInput = {
@@ -14,7 +16,10 @@ const TInput = {
   errorStatusClass: 'border-red-300 bg-red-100',
   warningStatusClass: 'border-yellow-400 bg-yellow-100',
   successStatusClass: 'border-green-300 bg-green-100',
-  disabledClass: 'bg-gray-100 cursor-not-allowed opacity-75'
+  disabledClass: 'bg-gray-100 cursor-not-allowed opacity-75',
+  defaultSizeClass: 'p-3',
+  largeSizeClass: 'p-4 text-lg',
+  smallSizeClass: 'p-2 text-sm'
 }
 
 const TTextarea = {
@@ -23,13 +28,26 @@ const TTextarea = {
   errorStatusClass: 'border-red-300 bg-red-100',
   warningStatusClass: 'border-yellow-400 bg-yellow-100',
   successStatusClass: 'border-green-300 bg-green-100',
-  disabledClass: 'bg-gray-100 cursor-not-allowed opacity-75'
+  disabledClass: 'bg-gray-100 cursor-not-allowed opacity-75',
+  defaultSizeClass: 'p-3',
+  largeSizeClass: 'p-4 text-lg',
+  smallSizeClass: 'p-2 text-sm'
 }
 
 const TSelect = {
   baseClass: 'block appearance-none w-full border pr-8 rounded leading-tight',
   baseClassMultiple: 'block appearance-none w-full border rounded leading-tight',
-  defaultStatusClass: 'bg-transparent border-primary'
+  defaultStatusClass: 'bg-transparent border-primary',
+  errorStatusClass: 'border-red-300 bg-red-100',
+  successStatusClass: 'border-green-300 bg-green-100',
+  warningStatusClass: 'border-yellow-400 bg-yellow-100',
+  disabledClass: 'bg-gray-100 cursor-not-allowed opacity-75',
+  defaultSizeClass: 'p-3',
+  largeSizeClass: 'p-4 text-lg',
+  smallSizeClass: 'p-2 text-sm',
+  wrapperClass: 'relative',
+  arrowWrapperClass: 'pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700',
+  arrowClass: 'fill-current h-4 w-4'
 }
 
 const TDropdown = {
