3a1e66def83cd2c37d29773f97c20f20080fd789
Philip Seifi <seifip@gmail.com>
2020-05-01 14:55:37 +0300

    long link wrap

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -119,4 +119,17 @@ export default {
   .note-menu
     position: relative
     margin-right: -10px
+
+  .markdown-body
+    /* These are technically the same, but use both */
+    overflow-wrap: break-word
+    word-wrap: break-word
+
+    /* This is the dangerous one in WebKit, as it breaks things wherever */
+    word-break: break-all
+    /* Instead use this non-standard one: */
+    word-break: break-word
+
+    /* Adds a hyphen where the word breaks, if supported (No Blink) */
+    hyphens: auto
 </style>
